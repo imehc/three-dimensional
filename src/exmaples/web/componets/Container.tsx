cc89e1ee2efcb5d5a3dc2a7705c958ad8ffe1f02
imehc <richohealthy@hotmail.com>
2025-12-25 15:11:51 +0800

    feat: 丰富container配置

--- src/exmaples/web/componets/Container.tsx
+++ src/exmaples/web/componets/Container.tsx
@@ -1,21 +1,30 @@
+import ErrorBoundary from "@docusaurus/ErrorBoundary";
 import clsx from "clsx";
 import { forwardRef, type PropsWithChildren } from "react";
 
 interface Props {
 	className?: string;
+	hiddenBorder?: boolean;
+	autoHeight?: boolean;
+	fullWidth?: boolean;
 }
 
 export default forwardRef<HTMLDivElement, PropsWithChildren<Props>>(
-	({ children, className }, ref) => {
+	({ children, className, hiddenBorder = false, autoHeight = false, fullWidth = false }, ref) => {
 		return (
 			<div
 				ref={ref}
 				className={clsx(
-					"tw:w-full tw:md:w-2xl tw:aspect-video tw:border tw:border-base-300 tw:box-border tw:relative",
+					"tw:w-full tw:relative",
+					{ 'tw:border tw:border-base-300 tw:box-border': !hiddenBorder },
+					{ 'tw:aspect-video': !autoHeight },
+					[fullWidth ? 'tw:w-full' : 'tw:md:w-2xl'],
 					className,
 				)}
 			>
-				{children}
+				<ErrorBoundary fallback={() => <div>Something went wrong</div>}>
+					{children}
+				</ErrorBoundary>
 			</div>
 		);
 	},
